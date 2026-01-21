b93bd26c0cdc3186f222c8f284c1d6f5c8be0173
TheLionCoder <eorlando.reyesruiz@gmail.com>
2026-01-21 15:57:19 -0500

    feat: Add bq_cleanup.sh script
    This script allows for the deletion of specified BigQuery tables. It includes safeguards such as a dry-run mode (enabled by default) and a confirmation flag to prevent accidental deletions.

---
+++ bq_cleanup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+PROJECT=$1
+DATASET=$2
+TABLES=$3
+DRY_RUN=${4:-true}
+SURE=${5:-false}
+
+if [[ -z "$PROJECT" || -z "$DATASET" || -z "$TABLES" ]]; then
+  echo "Usage: $0 <PROJECT> <DATASET> <TABLES> <DRY_RUN> <SURE>"
+  exit 1
+fi
+
+if [ "$DRY_RUN" = "true" ]; then
+  echo "--DRY_RUN Mode (No changes will made)--"
+fi
+
+if [ "$SURE" != "true" ] && [ "$DRY_RUN" = "false" ]; then
+  echo "ERROR: Actual deletion requires SURE=true flag."
+  exit 1
+fi
+
+for table in $TABLES; do
+  FULL_PATH="$PROJECT:$DATASET.$table"
+
+  if bq show $$FULL_PATH >/dev/null 2>&1; then
+    if [ "$DRY_RUN" = "true" ]; then
+      echo "[ DRY_RUN ] wold delete $FULL_PATH"
+    else
+      echo "Deleting $FULL_PATH..."
+      bq rm -f "$FULL_PATH"
+    fi
+  else
+    echo "SKIPPING: $FULL_PATH does not exist."
+  fi
+done
+echo "Process completed Successfully"
